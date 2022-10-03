03dcec05a09a6066f7962bed84278c5106f79767
raflirahman <raflirahman375@gmail.com>
2022-10-03 10:52:24 +0700

    add tugas 15

---
+++ tugas15.js
@@ -0,0 +1,6 @@
+var AB = 8;
+var BC = 6;
+var hasil = Math.sqrt(Math.pow(AB,2)+Math.pow(BC,2));
+console.log('Nilai AB : 8');
+console.log('Nilai AB : 6');
+console.log('panjang sisi AC pada segitiga siku siku tersebut adalah :',hasil,'cm');
